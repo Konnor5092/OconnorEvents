f9adb2a21e7d5fc5ae9d915db8f72d587ecd81c5
Matt O'Connor <matty_o_connor01@hotmail.com>
2021-04-08 17:43:20 +0100

    Adding entity framework

---
+++ OconnorEvents.EventCatalog/DbContext/SchoolContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OconnorEvents.EventCatalog.DbContext
+{
+    public class SchoolContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+    }
+}
---
+++ OconnorEvents.EventCatalog/Dtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OconnorEvents.EventCatalog.Dtos
+{
+    public class CategoryDto
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
